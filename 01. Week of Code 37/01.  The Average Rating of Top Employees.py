0da39d9c110f1bc8e66c68e504ad54805a9380eb
Alexander Marinskiy <marinskiy@gmail.com>
2018-04-25 10:34:00 +0300

    add 01. The Average Rating of Top Employees

---
+++ 01. Week of Code 37/01.  The Average Rating of Top Employees.py
@@ -0,0 +1,20 @@
+def averageOfTopEmployees(rating):
+    average = roundi(sum(rating) / len(rating) * 100) / 100
+    print('%.2f' % average)
+
+
+def roundi(a):
+    if a % 1 >= 0.5:
+        a = a // 1 + 1
+    else:
+        a = a // 1
+    return a
+
+
+n = int(input())
+rating = []
+for i in range(n):
+    rating_item = int(input())
+    if rating_item >= 90:
+        rating.append(rating_item)
+averageOfTopEmployees(rating)
