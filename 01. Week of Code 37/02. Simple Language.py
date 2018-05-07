dc59c48bdfd94bdf93f347a37a47de42debb7826
Alexander Marinskiy <marinskiy@gmail.com>
2018-05-07 10:44:39 +0300

    add 02. Simple Language

---
+++ 01. Week of Code 37/02. Simple Language.py
@@ -0,0 +1,17 @@
+# This is the solution to https://www.hackerrank.com/contests/w37/challenges/simple-language/problem
+
+
+def maximumProgramValue(n):
+    maxValue = 0
+    for i in range(n):
+        string = list(input().split())
+        if string[0] == 'set':
+            maxValue = max(int(string[1]), maxValue)
+        elif string[0] == 'add':
+            maxValue += max(int(string[1]), 0)
+    return maxValue
+
+
+n = int(input())
+result = maximumProgramValue(n)
+print(result)
