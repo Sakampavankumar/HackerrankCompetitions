c783226d8708490b5ec788b9e55feb78ebb097b3
marinskiy <marinskiy@gmail.com>
2019-01-08 14:32:45 +0300

    add 001. Hanging Posters

---
+++ 02. HourRank 31/001. Hanging Posters.py
@@ -0,0 +1,9 @@
+# This is the solution to https://www.hackerrank.com/contests/hourrank-31/challenges/hanging-posters/problem
+
+
+import math
+n, h = map(int, input().split())
+wallPoints = list(map(int, input().rstrip().split()))
+lengths = list(map(int, input().rstrip().split()))
+ans_arr = [wallPoints[i] - lengths[i]*0.25 for i in range(len(wallPoints))]
+print(max(math.ceil(max(ans_arr)) - h, 0))
